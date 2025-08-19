68c8b7170235204996acfc7620d6073352dad5da
Artur-Mac <arturmac10@gmail.com>
2025-08-19 05:22:46 -0300

    finalizando desafio abstração de um celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+// (string numero, string modelo, string imei, int memoria)
+Iphone iphone1 = new Iphone("402394023", "modelo lorem", "imei: 423432423", 256);
+
+Nokia nokia1 = new Nokia("312312312", "modelo ipsum", "423423423423", 4);
+
+nokia1.InstalarAplicativo("Wahtasapp");
+
+Console.WriteLine(nokia1.Modelo);
+
+nokia1.ReceberLigacao();
+
+
+iphone1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine(iphone1.Modelo);
+
+iphone1.ReceberLigacao();
+
