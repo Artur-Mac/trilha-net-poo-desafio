a4a16b63c2da5f3c6893a76600b88a9b5d43ea37
Artur-Mac <arturmac10@gmail.com>
2025-08-19 05:25:00 -0300

    funcionalidades finais: abstração implementada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Iphone iphone1 = new Iphone("402394023", "modelo lorem", "imei: 423432423", 256)
 
 Nokia nokia1 = new Nokia("312312312", "modelo ipsum", "423423423423", 4);
 
-nokia1.InstalarAplicativo("Wahtasapp");
+nokia1.InstalarAplicativo("Whatsapp");
 
 Console.WriteLine(nokia1.Modelo);
 
